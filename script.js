2872e11300af5fd3b253841a3512af7e9dd957bd
Pherwerz <51902126+Pherwerz@users.noreply.github.com>
2020-04-16 13:57:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() {
    //variables
-   var num, opp, del, numArr, opArr, screen, history, fill, answer, pressedkey, press, conNumber, total, clear, equals;
+   var num, opp, del, screen, history, answer, pressedkey, press, clear, equals;
    num = document.querySelectorAll('#num');
    opp = document.querySelectorAll('#op');
    screen = document.querySelector('#small');
@@ -8,29 +8,22 @@
    clear = document.querySelector('#clear');
    equals = document.querySelector('#equals');
    history = document.querySelector('#history');
-   fill = document.querySelector('#fill');
    del = document.querySelector('#del')
    pressedkey = '';
    press = '';
-   numArr = [];
-   opArr = [];
-   conNumber = 0;
-   total = 0;
    
    var button = {
       numbersPress: function(i) {
          num[i].addEventListener('click', function(){
             if (answer.textContent !== ''){
                answer.textContent = '';
-               total = 0;
+               press = '';
                screen.textContent = '';
                pressedkey = '';
             }
             // 1 save pressed key
             pressedkey += num[i].value;
             press += num[i].value;
-            // 2 convert string to number
-            conNumber = parseFloat(press);
             // 3 render pressedkey to ui
             screen.textContent = pressedkey;
          })
@@ -39,10 +32,13 @@
          opp[i].addEventListener('click', function() {
             pressedkey += ' ' + opp[i].value + ' ';
             screen.textContent = pressedkey;
-            numArr.push(conNumber);
-            opArr.push(opp[i].value);
-            conNumber = 0;
-            press = '';
+            if (opp[i].value === '×') {
+               press += ' * ';
+            } else if (opp[i].value === '÷') {
+               press += ' / ';
+            } else {
+               press += ' ' + opp[i].value + ' ';
+            }
          })
       }
    };
@@ -59,61 +55,27 @@
    clear.addEventListener('click', function() {
       pressedkey = '';
       press = '';
-      total = 0;
-      conNumber = 0;
-      numArr = [];
-      opArr = [];
       screen.textContent = null;
       answer.textContent = null;
    });
    
    // equals button
    equals.addEventListener('click', function() {
-      if (numArr.length === opArr.length) {
-         numArr.push(conNumber);
-      }
-      total = numArr[0] * 10;
-      if (numArr.length > 1) {
-         for (var n = 0; n < opArr.length; n++) {
-            if (opArr[n] === '+') {
-               total += (numArr[n + 1] * 10);
-            } else if (opArr[n] === '-') {
-               total -= (numArr[n + 1] * 10);
-            } else if (opArr[n] === '×') {
-               total *= (numArr[n + 1] * 10);
-            } else if (opArr[n] === '÷') {
-               total /= (numArr[n + 1] * 10);
-            }
-         }
-      };
-      var markup = '<hr><div class="screen"><div class="row">'+pressedkey+'</div><div class="row2">'+total/10+'</div></div>';
+      answer.textContent = eval(press);
+      var markup = '<hr><div class="screen"><div class="row">'+pressedkey+'</div><div class="row2">'+eval(press)+'</div></div>';
       history.insertAdjacentHTML('beforeend', markup);
-      conNumber = 0;
-      press = '';
-      answer.textContent = total / 10;
    });
    
    // clear one
    del.addEventListener('click', function() {
-      if (conNumber !== 0) {
-         numArr.push(conNumber);
-         conNumber = 0;
-         pressed = '';
-      }
       if (pressedkey.endsWith(' ')) {
          pressedkey = pressedkey.substring(0, pressedkey.length - 3);
+         press = press.substring(0, press.length - 3);
          screen.textContent = pressedkey;
-         opArr.pop();
       } else {
          pressedkey = pressedkey.substring(0, pressedkey.length - 1);
+         press = press.substring(0, press.length - 1);
          screen.textContent = pressedkey;
-         var lastNum = numArr[numArr.length - 1].toString();
-         numArr.pop();
-         if (lastNum.length > 1) {
-            lastNum = lastNum.substring(0, lastNum.length - 1);
-            lastNum = parseFloat(lastNum);
-            numArr.push(lastNum);
-         }
       }
    });
    
@@ -124,5 +86,4 @@
    document.querySelector('.history-button').addEventListener('click', function(){
       document.querySelector('.history').style.display = 'none';
    });
-   
 })();
